93a9ff697a4f27aebde416283a0708585270f22a
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-07-04 02:41:43 +0000

    Best config yet

--- train_deep_fashion.sh
+++ train_deep_fashion.sh
@@ -1,5 +1,11 @@
 python3 train.py \
-    --train_set data/consumer2shop_deepfashion.csv \
-    --image_root /home/linus/Downloads/Consumer-to-shop_Clothes_Retrieval_Benchmark/Img/ \
-    --experiment_root /home/linus/fashion-embedding/experiments/default_param/ \
-    --batch_p 16
+    --train_set /data/linus/data/deep_fashion_2.csv \
+    --image_root /data/linus/data/images/ \
+    --experiment_root /data/linus/triplet-reid/final \
+    --embedding_dim 512 \
+    --batch_k 4 \
+    --batch_p 3 \
+    --net_input_height 512 \
+    --net_input_width 512 \
+    --train_iterations 100000 \
+    --detailed_logs
